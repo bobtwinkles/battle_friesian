bb38bcdbbfb567fe518f8682b5f626ba3543c26b
bobtwinkles <srkoser+GitHub@gmail.com>
2015-01-17 17:49:06 -0500

    make scales floats

--- systems/Scales.hpp
+++ systems/Scales.hpp
@@ -2,10 +2,10 @@
 #define _SYSTEM_SCALES_H_
 
 // Scale of phyiscs units to screen units
-#define PHYS_SCALE 10
+#define PHYS_SCALE 10.0
 // Scale of screen units to screen units
-#define GFX_SCALE 1
+#define GFX_SCALE 1.0
 // Scale of image pixel to screen units
-#define IMG_SCALE 1366
+#define IMG_SCALE 1366.0
 
 #endif
