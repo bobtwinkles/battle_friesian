7396ff4c976cce9744b564ff5abca8d878d874fa
bobtwinkles <srkoser+GitHub@gmail.com>
2015-01-31 20:20:16 -0500

    Convert Platform to use new Physics infrastructure

--- systems/entity/entities/Platform.cpp
+++ systems/entity/entities/Platform.cpp
@@ -2,36 +2,117 @@
 
 #include "systems/GameContext.hpp"
 #include "systems/Scales.hpp"
+#include "systems/physics/Fixture.hpp"
 
 using fri::ogl::Texture;
+using fri::util::rtti::TypeID;
+using fri::util::rtti::NextTypeID;
 using fri::system::GameContext;
 using fri::system::entity::entities::Platform;
+using fri::system::entity::entities::platform::PlatformBody;
+using fri::system::entity::entities::platform::PlatformFixture;
+using fri::system::entity::entities::platform::PlatformSide;
 using fri::system::render::MobileTexturedRenderer;
+
+TypeID PlatformBody::_type = NextTypeID();
+TypeID PlatformFixture::_type = NextTypeID();
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////// Main Platform members ///////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 Platform::Platform(GameContext & Context, float X, float Y, float W, float H, std::shared_ptr<Texture> Tex) {
   //TODO: implement rendering
   assert(Tex == nullptr);
 
-  float half_w = W * PHYS_SCALE / GFX_SCALE / 2;
-  float half_h = H * PHYS_SCALE / GFX_SCALE / 2;
-
-  b2BodyDef def;
   b2World & world = Context.GetPhysicsSystem().GetWorld();
-  def.type = b2_staticBody;
-  def.position.Set(half_w + X * PHYS_SCALE / GFX_SCALE, half_h + Y * PHYS_SCALE / GFX_SCALE);
-  _body =world.CreateBody(&def);
+  _x = X;
+  _y = Y;
+  _width = W;
+  _height = H;
+  _object = new PlatformBody(world, this);
 
-  b2PolygonShape box;
-  box.SetAsBox(half_w, half_h);
-
-  b2FixtureDef fixture;
-  fixture.shape = &box;
-  fixture.friction = 0.5f;
-
-  _body->CreateFixture(&fixture);
+  _object->AttachFixture(new PlatformFixture(_object, PlatformSide::TOP));
+  _object->AttachFixture(new PlatformFixture(_object, PlatformSide::BOTTOM));
+  _object->AttachFixture(new PlatformFixture(_object, PlatformSide::LEFT));
+  _object->AttachFixture(new PlatformFixture(_object, PlatformSide::RIGHT));
 }
 
 Platform::~Platform() {
+  delete _object;
 }
+
 void Platform::Tick(GameContext & Context, double Step) {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////// Physics Platform members ///////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////// PlatformBody //////////////////////////////////////
+PlatformBody::PlatformBody(b2World & World, Platform * Plat) : PhysicsObject( { this, _type} ) {
+  _platform = Plat;
+
+  float x = Plat->GetX() * PHYS_SCALE / GFX_SCALE
+      , y = Plat->GetY() * PHYS_SCALE / GFX_SCALE
+      , w = Plat->GetWidth() * PHYS_SCALE / GFX_SCALE
+      , h = Plat->GetHeight() * PHYS_SCALE / GFX_SCALE;
+
+  b2BodyDef def;
+  def.type = b2_staticBody;
+  def.position.Set((w / 2) + x * PHYS_SCALE / GFX_SCALE, (h / 2) + y * PHYS_SCALE / GFX_SCALE);
+  def.userData = this->GetStub();
+  _body = World.CreateBody(&def);
+}
+
+PlatformBody::~PlatformBody() {
+}
+
+//////////////////////////// PlatformFixture ///////////////////////////////////
+
+PlatformFixture::PlatformFixture(PlatformBody * Parent, PlatformSide Side) : Fixture( {this, _type} ) {
+  Platform * plat = Parent->GetPlatform();
+  float px = plat->GetX() * PHYS_SCALE / GFX_SCALE
+      , py = plat->GetY() * PHYS_SCALE / GFX_SCALE
+      , pw = plat->GetWidth() * PHYS_SCALE / GFX_SCALE
+      , ph = plat->GetHeight() * PHYS_SCALE / GFX_SCALE;
+
+  float w = pw * 0.1
+      , h = ph * 0.1;
+
+  float x, y;
+  b2PolygonShape box;
+
+  switch (Side) {
+    case PlatformSide::TOP:
+      x = 0;
+      y = (ph - h) / 2;
+      box.SetAsBox(pw, h, b2Vec2(x, y), 0);
+      break;
+    case PlatformSide::BOTTOM:
+      x = 0;
+      y = (-ph + h) / 2;
+      box.SetAsBox(pw, h, b2Vec2(x, y), 0);
+      break;
+    case PlatformSide::LEFT:
+      x = -pw + w;
+      y = 0;
+      box.SetAsBox(w, ph / 2 - h, b2Vec2(x, y), 0);
+      break;
+    case PlatformSide::RIGHT:
+      x = pw - w;
+      y = 0;
+      box.SetAsBox(w, ph / 2 - h, b2Vec2(x, y), 0);
+      break;
+  }
+
+
+  b2FixtureDef def;
+  def.shape = &box;
+  def.friction = 0.1;
+  def.density = 1;
+  def.userData = this->GetStub();
+
+  Parent->GetParent()->CreateFixture(&def);
 }
--- systems/entity/entities/Platform.hpp
+++ systems/entity/entities/Platform.hpp
@@ -1,24 +1,91 @@
 #ifndef _SYSTEMS_ENTITY_ENTITIES_PLATFORM_H_
 #define _SYSTEMS_ENTITY_ENTITIES_PLATFORM_H_
 
-#include <Box2D/Box2D.h>
-
 #include "systems/entity/GameObject.hpp"
 
 #include "systems/render/RenderSystem.hpp"
 #include "systems/render/renderables/MobileTexturedRender.hpp"
 
+#include "systems/physics/PhysicsObject.hpp"
+#include "systems/physics/Fixture.hpp"
+
 namespace fri {
   namespace system {
     namespace entity {
       namespace entities {
+        class Platform;
+
+        namespace platform {
+          enum PlatformSide {
+            TOP, BOTTOM, LEFT, RIGHT
+          };
+
+          /**
+           * A platform body
+           */
+          class PlatformBody : public fri::system::physics::PhysicsObject {
+            private:
+              Platform * _platform;
+            public:
+              static fri::util::rtti::TypeID _type;
+
+              /**
+               * Creates a PlatformBody.
+               *
+               * Does _not_ manage the Platform passed in.
+               *
+               * @param World
+               * @param Plat The platform for which we are a physics body.
+               */
+              PlatformBody(b2World & World, Platform * Plat);
+
+              inline Platform * GetPlatform() const { return _platform; }
+              virtual ~PlatformBody();
+          };
+
+          /**
+           * Represents a fixture on a platform.
+           *
+           * Objects of this class type will represent only one side of the platform.
+           * To createa complete platform, this class must be instantiated with sides
+           * `PlatformSide::TOP`, `PlatformSide::BOTTOM`, `PlatformSide::RIGHT`, and `PlatformSide::LEFT`.
+           */
+          class PlatformFixture : public fri::system::physics::Fixture {
+            private:
+              PlatformSide _side;
+            public:
+              static fri::util::rtti::TypeID _type;
+
+              /**
+               * Create a platform fixture.
+               *
+               * @param Parent
+               * @param W The Width of the platform
+               * @param H The Height of the platform
+               * @param Side the Side for which we will create
+               */
+              PlatformFixture(PlatformBody * Parent, PlatformSide Side);
+
+              PlatformSide GetSide() { return _side; }
+          };
+        }
+
+        /**
+         * A static, rectangular platform upon which things can land
+         */
         class Platform : public IGameObject {
           private:
             fri::system::render::RenderableIndex _renderable_index;
-            b2Body * _body;
+            platform::PlatformBody * _object;
+            float _x, _y, _width, _height;
           public:
             Platform(fri::system::GameContext & Context, float X, float Y, float W, float H, std::shared_ptr<fri::ogl::Texture> Tex);
             ~Platform();
+
+            inline float GetX() const { return _x; }
+            inline float GetY() const { return _y; }
+            inline float GetWidth() const { return _width; }
+            inline float GetHeight() const { return _height; }
 
             virtual void Tick(fri::system::GameContext & Context, double Step);
         };
